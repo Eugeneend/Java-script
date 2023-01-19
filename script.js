c8a13a3f9ac98e0725c462aefa90479da99f4619
Eugene <Oleksienko2003@gmail.com>
2023-01-19 20:44:42 +0200

    Javascript

---
+++ script.js
@@ -0,0 +1,105 @@
+
+'use strict'
+
+// alert('Сценарій, підключення з звонішнього файлу. ');
+
+//  var змінна але не викор.
+/* let службове слово.
+ const змінні */
+
+//  let nameUserProfile, nameUserPhoto, lastName;
+
+// let nameUserProfile;
+// let nameUserPhoto;
+// let lastName;
+
+/*let nameUser;
+
+nameUser = prompt('Введіть своє імя');
+
+console.log(nameUser);*/
+
+// let nameUser;
+// console.log(typeof nameUser);
+
+// nameUser = '23' ;
+
+// console.log(typeof nameUser);
+
+// nameUser = false;
+
+// console.log(typeof nameUser);
+
+// nameUser = true;
+
+// console.log(typeof nameUser);
+
+
+// nameUser = null;
+
+// console.log(typeof nameUser);
+
+/*  Оператори арефметичні
+ - 
+ +
+ *
+ /
+ %
+ */
+
+/*let a, b;
+
+a = 13;
+b = 6;
+
+let c = a + b;
+
+console.log(a / b);
+
+console.log(a % b)
+
+console.log(--a);
+
+console.log(a);*/
+
+/* 
+
+> чи більше. 
+< чи менше.
+== неявне претворення типів.
+=== лише порівняння без перетворення.
+>=
+<=
+!=
+
+*/
+/*let a =7; //число
+let b ='7'; //текст
+let c = a + b;
+
+console.log(typeof c);
+console.log(typeof a+b); // склейка // * / - намагається змінити на число.
+*/
+
+let userName = prompt(`Як ваше ім\'я?`); // - \ -екранування
+let age = prompt('Скільки вам років?')  // promt - рядок
+
+console.log(' Доброго дня ' + userName + '!'); // Eugeконкотинація-обьеднання
+console.log(' Вам ' + age + " років ");
+
+let newAge = +age + 5; // +age - перетворили на число .
+console.log(' Через 5 років вам буде ' + newAge ); 
+console.log(`Через 5 вам буде ${+age + 5} років ` ); //більш правильно ніж це ^
+
+// console.log(' Через 5 ' + age + " років вам буде " + newAge);
+
+
+
+
+
+
+
+
+
+
+
